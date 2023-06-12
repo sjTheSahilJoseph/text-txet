9a62af0380318872a59e07e1396c871eb3e8aac8
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2023-06-12 05:21:06 +0500

    Remove Extra Spaces, Words, Lines, Character Count

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -36,7 +36,11 @@ function Textform(props) {
         }
         setText(tempTextLineBreak);
       };
-      
+
+      const handleRemoveExtraSpacesClick = () => {
+        const newText = text.replace(/[^\S\r\n]+/g, ' ');
+        setText(newText);
+      }
 
     const handleClearClick = () => {
         setText("");
@@ -46,11 +50,13 @@ function Textform(props) {
         <div className="container " style={{backgroundColor: props.mode==='dark'?'#0A1648':null, color: props.mode==='dark'?'#ffffff':'black'}}>
             <div className="my-3">
                 <h2>Enter the Text : </h2>
+                <p><b>Characters</b> : {text.length} - <b>Words</b> : {text.split(/\s+/).filter(word => word.length > 0).length} - <b>Lines</b> : {text.split(/\r\n|\r|\n/).filter(line => line.trim().length > 0).length || 0}</p>
                 <textarea onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#0A1648':null, color: props.mode==='dark'?'#ffffff':'#0A1648'}} value={text} className="form-control" id="textForm" rows="8" placeholder='Write Here ... '></textarea>
                 <button onClick={handleUpperClick} type="button" className="btn btn-primary my-3 mx-1">UPPERCASE</button>
                 <button onClick={handleLowerClick} type="button" className="btn btn-primary my-3 mx-1">lowercase</button>
                 <button onClick={handleCapatalizeClick} type="button" className="btn btn-primary my-3 mx-1">Capatalize</button>
                 <button onClick={handleLineBreaksClick} type="button" className="btn btn-primary my-3 mx-1">Remove Line Breaks</button>
+                <button onClick={handleRemoveExtraSpacesClick} type="button" className="btn btn-primary my-3 mx-1">Remove Extra Spaces</button>
                 <button onClick={handleClearClick} type="button" className="btn btn-danger my-3 mx-1">Clear</button>
             </div>
         </div>
