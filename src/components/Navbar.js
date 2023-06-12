0619fcfd1a8b056b3b1810fa6bb4b57614008d36
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2023-06-12 07:01:04 +0500

    done done done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg  navbar-${props.mode==='dark'?'dark':'light'} bg-${props.mode==='dark'?'dark':'light'}`}>
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">textTxet</a>
+        <a className="navbar-brand" href="https://sjthesahiljoseph.github.io/text-txet/">textTxet</a>
         <div className="form-check form-switch">
   <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
   <label className={`form-check-label text-${props.mode==='dark'?'light':'dark'}`} htmlFor="flexSwitchCheckDefault">Enable {`${props.mode==='dark'?'Light':'Dark'}`} Mode</label>
